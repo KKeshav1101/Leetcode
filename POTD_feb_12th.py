76747a46342ec23aabb1148d18fb116a18504515
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-02-12 22:48:00 +0530

    Create POTD_feb_12th.py

---
+++ POTD_feb_12th.py
@@ -0,0 +1,10 @@
+#potd feb 12th- majority element
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in nums:
+            if ((nums.count(i))>(len(nums)//2)):
+                return i
