0b070bd6728fdd8fc242fd9ef37678a301339d51
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-02-13 23:00:01 +0530

    Create SearchInsertPosition.py

---
+++ SearchInsertPosition.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.append(target)
+        nums.sort()
+        return(nums.index(target))
