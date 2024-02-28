7cad3c7146ba28d751ecdaf5fe921c6cf43ebe08
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-02-28 23:24:09 +0530

    Create ValidPalindrome.py

---
+++ ValidPalindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s):
+        l=0
+        r=len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l+=1
+            elif not s[r].isalnum():
+                r-=1
+            elif s[l].lower() == s[r].lower():
+                l+=1
+                r-=1
+            else:
+                return False
+
+        return True
+            
