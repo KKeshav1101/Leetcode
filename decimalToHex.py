eed906993cbf6f2ebc572b869151b9a2f0a533fd
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-03-12 23:23:56 +0530

    Create decimalToHex.py

---
+++ decimalToHex.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num==0:
+            return '0'
+        if num<0:
+            num=(1<<32)+num
+        hex_digits='0123456789abcdef'
+        hex_num=''
+        while num>0:
+            digit=num%16
+            hex_digit=hex_digits[digit]
+            hex_num=hex_digit+hex_num
+            num//=16
+        return hex_num
