d6e29bb3116f5b41c4b1d9c8695c92acb51db9de
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-06-06 22:28:58 +0530

    Create isNStraightHand.py

---
+++ isNStraightHand.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        """
+        :type hand: List[int]
+        :type groupSize: int
+        :rtype: bool
+        """
+        if len(hand) % groupSize != 0:
+            return False
+
+        card_count = Counter(hand)
+        sorted_cards = sorted(card_count.keys())
+
+        for card in sorted_cards:
+            if card_count[card] > 0:
+                count = card_count[card]
+                for i in range(groupSize):
+                    if card_count[card + i] < count:
+                        return False
+                    card_count[card + i] -= count
+
+        return True
