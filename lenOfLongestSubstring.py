b2c2544edb00131322bfdb420c285ae95654a5c6
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-06-10 13:15:26 +0530

    Create lenOfLongestSubstring.py

---
+++ lenOfLongestSubstring.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left =0
+        max_length = 0
+        char_set = set()
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left+=1
+            char_set.add(s[right])
+            max_length = max(max_length, right-left+1)
+        return max_length
+        
