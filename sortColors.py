7343d377cc785299b931e964c198f47bfafb7be5
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-06-12 13:40:05 +0530

    Counting sort update

--- sortColors.py
+++ sortColors.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        freq=[0]*3
+        for x in nums: freq[x]+=1
+        count=0
+        for x in range(3):
+            nums[count:count+freq[x]] = [x]*freq[x]
+            count+= freq[x]
