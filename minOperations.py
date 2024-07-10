03909d3088030a369848ee654d36209bc5aa4ffc
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-07-10 16:25:02 +0530

    potd 10th july

---
+++ minOperations.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        stack = []
+        for log in logs:
+            if log=='../':
+                if stack:
+                    stack.pop()
+            elif log=="./":
+                continue
+            else:
+                stack.append(log)
+        return len(stack)
