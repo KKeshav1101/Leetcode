9e797da568415731562f22fb1e4e899b7582e440
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-07-15 21:47:08 +0530

    potd 15th july

---
+++ createBinaryTree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def createBinaryTree(self, descriptions):
+        """
+        :type descriptions: List[List[int]]
+        :rtype: Optional[TreeNode]
+        """
+        mp={}
+        hasParent = set()
+        for desc in descriptions:
+            if desc[0] not in mp:
+                mp[desc[0]] = TreeNode(desc[0])
+            if desc[1] not in mp:
+                mp[desc[1]] = TreeNode(desc[1])
+            hasParent.add(desc[1])
+
+        root = None
+        for desc in descriptions:
+            if desc[2] == 1:
+                mp[desc[0]].left=mp[desc[1]]
+            else:
+                mp[desc[0]].right=mp[desc[1]]
+            if desc[0] not in hasParent:
+                root = mp[desc[0]]
+
+        return root
