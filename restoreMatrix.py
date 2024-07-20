4484eb640b016c2501a94f6a60fb24a30f5800e3
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-07-20 21:42:08 +0530

    Create restoreMatrix.py

---
+++ restoreMatrix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def restoreMatrix(self, rowSum, colSum):
+        """
+        :type rowSum: List[int]
+        :type colSum: List[int]
+        :rtype: List[List[int]]
+        """
+        r,c = len(rowSum),len(colSum)
+        arr=[[0]*c for _ in range(r)]
+        i,j=0,0
+        while i<r and j<c:
+            x=min(rowSum[i],colSum[j])
+            arr[i][j]=x
+            rowSum[i]-=x
+            colSum[j]-=x
+            i+=(rowSum[i]==0)
+            j+=(colSum[j]==0)
+        return arr
