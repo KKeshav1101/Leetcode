9405864ad98d8494b95a59ad53413d485f3e6b82
K Keshav <122164516+KKeshav1101@users.noreply.github.com>
2024-07-29 21:10:47 +0530

    potd 29-7-24

---
+++ numTeams.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def numTeams(self, rating):
+        """
+        :type rating: List[int]
+        :rtype: int
+        """
+        total = 0
+        n = len(rating)
+        for i in range(n):
+            right_less = 0
+            right_more = 0
+            left_less = 0
+            left_more = 0
+
+            for j in range(i+1,n):
+                if rating[j]<rating[i]:
+                    right_less += 1
+                elif rating[j]> rating[i]:
+                    right_more += 1
+            
+            for j in range(i):
+                if rating[j]<rating[i]:
+                    left_less += 1
+                elif rating[j]>rating[i]:
+                    left_more += 1
+
+            total += right_less*left_more + right_more * left_less
+        
+        return total
