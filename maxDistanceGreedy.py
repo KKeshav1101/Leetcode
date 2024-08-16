5bf2ed6987f7eff576218f90e4ea032cbb2e4391
K Keshav <126003131@sastra.ac.in>
2024-08-16 23:01:48 +0530

    Create maxDistanceGreedy.py

---
+++ maxDistanceGreedy.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxDistance(self, arrays):
+        """
+        :type arrays: List[List[int]]
+        :rtype: int
+        """
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = 0
+        for i in range(1,len(arrays)):
+            arr=arrays[i]
+            max_distance = max(max_distance,abs(arr[-1]-smallest),abs(biggest-arr[0]))
+            smallest=min(smallest,arr[0])
+            biggest=max(biggest,arr[-1])
+        return max_distance
+        
